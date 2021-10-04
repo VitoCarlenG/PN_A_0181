44d6323fabc7318b6d8fa7f96cd30a276c1c03de
VitoCarlenG <xlhvitocarlen@gmail.com>
2021-10-04 07:54:42 +0700

    PN_A_0181

--- app/src/main/java/com/example/pn_a_0181/MyApplication.java
+++ app/src/main/java/com/example/pn_a_0181/MyApplication.java
@@ -1,15 +1,30 @@
 package com.example.pn_a_0181;
 
 import android.app.Application;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
+import android.os.Message;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
-public class MyApplication extends Application {
+public class MyApplication extends FirebaseMessagingService {
     public static final String CHANNEL_1_ID = "channel1";
     public static final String CHANNEL_2_ID = "channel2";
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        String title=remoteMessage.getNotification().getTitle();
+        String text=remoteMessage.getNotification().getBody();
+        final String CHANNEL_ID="HEADS_UP_NOTIFICATION";
+        super.onMessageReceived(remoteMessage);
+    }
 
     @Override
     public void onCreate() {
